d20fb07f693ce2af9660858652fb6adff0f357d1
alyaridwan27 <alyanursalmaridwan@gmail.com>
2025-11-15 15:20:08 +0700

    feat: Implement instrumentation using @vercel/otel

--- next.config.ts
+++ next.config.ts
@@ -3,25 +3,6 @@ import type { Configuration } from 'webpack';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-
-  webpack(config: Configuration, context: { isServer: boolean }) {
-    const { isServer } = context;
-
-    if (!isServer) {
-      config.resolve = config.resolve || {};
-      config.resolve.fallback = {
-        ...(config.resolve.fallback || {}),
-        stream: false,
-        net: false,
-        tls: false,
-        events: false,
-        fs: false,
-        http2: false,
-      };
-    }
-
-    return config;
-  },
 };
 
 export default nextConfig;
